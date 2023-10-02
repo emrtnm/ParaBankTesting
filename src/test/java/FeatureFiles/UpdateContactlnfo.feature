c5e52229c13be364a02406e51c724f25dc3664eb
SAMET ÇAMOĞLU <cmglsmt@gmail.com>
2023-10-02 22:38:49 +0300

    UpdateContactlnfo

---
+++ src/test/java/FeatureFiles/UpdateContactlnfo.feature
@@ -0,0 +1,64 @@
+Feature: Login Functionality
+
+  Scenario: Login with success
+    Given Navigate to website
+    And Enter username and password and click login button
+    Then I should login with successfully
+
+    And Click  the buttons UpdateContactlnfo
+      | clickButton |
+
+    And Clear and user informations UpdateContactlnfo
+      | name    |
+      | surName |
+      | address |
+      | city    |
+      | state   |
+      | zipcod  |
+      | phone   |
+
+    And  Write sendkeys user informations UpdateContactlnfo
+      | firstname   | zaha          |
+      | lastName    | cabbar        |
+      | Address     | GS sporkulubu |
+      | cty         | Istanbul      |
+      | State       | Seyrantepe    |
+      | zipcode     | 3452          |
+      | phoneNumber | 0000001905    |
+
+    And Click  the buttons UpdateContactlnfo
+      | updateButton |
+
+    Then  Success message profile updated
+
+    And Click  the buttons UpdateContactlnfo
+    | logout |
+
+    Given Navigate to website
+    And Enter username and password and click login button
+
+    Then I should login with successfully
+
+    Then Success message update verification
+
+    And Click  the buttons UpdateContactlnfo
+      | logout |
+
+  Scenario:Enter user informations  Negatif
+    Given Navigate to website
+    And Enter username and password and click login button
+    Then I should login with successfully
+
+    And Click  the buttons UpdateContactlnfo
+      | clickButton |
+
+    And Clear and user informations UpdateContactlnfo
+      | name    |
+      | city    |
+      | zipcod  |
+    Then Warning Message required fields
+
+    And Click  the buttons UpdateContactlnfo
+      | updateButton |
+
+    Then  Empty message profile not updated
---
+++ src/test/java/StepDefinitions/UpdateContactlnfo.java
@@ -0,0 +1,74 @@
+package StepDefinitions;
+
+import Pages.UpdateContactlnfoPom;
+import Utilities.Events;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class UpdateContactlnfo {
+    UpdateContactlnfoPom elements = new UpdateContactlnfoPom();
+
+
+    @And("Click  the buttons UpdateContactlnfo")
+    public void clickTheButtonsUpdateContactlnfo(DataTable buttons) {
+
+        List<String> strButtonsList = buttons.asList(String.class);
+
+        for (int i = 0; i < strButtonsList.size(); i++) {
+            WebElement linkWebElement = elements.getWebelement(strButtonsList.get(i));
+            Events.click(linkWebElement);
+
+
+        }
+    }
+
+    @And("Write sendkeys user informations UpdateContactlnfo")
+    public void writeSendkeysUserInformationsUpdateContactlnfo(DataTable keys) {
+
+        List<List<String>> items = keys.asLists(String.class);
+
+        for (int i = 0; i < items.size(); i++) {
+            WebElement e = elements.getWebelement(items.get(i).get(0));
+            String yazi = items.get(i).get(1);
+
+            Events.sendKeys(e, yazi);
+
+
+        }
+    }
+
+    @And("Clear and user informations UpdateContactlnfo")
+    public void clearAndUserInformationsUpdateContactlnfo(DataTable delet) {
+
+        List<String> strButtonsList = delet.asList(String.class);
+
+        for (int i = 0; i < strButtonsList.size(); i++) {
+            WebElement linkWebElement = elements.getWebelement(strButtonsList.get(i));
+            linkWebElement.clear();
+        }
+    }
+
+    @Then("Success message profile updated")
+    public void successMessageProfileUpdated() {
+        Events.verifyContainsText(elements.succassesMessage, "Profile Updated");
+    }
+
+    @Then("Success message update verification")
+    public void successMessageUpdateVerification() {
+        Events.verifyContainsText(elements.smallText, "zaha");
+    }
+
+    @Then("Warning Message required fields")
+    public void warningMessageRequiredFields() {
+        Events.verifyContainsText(elements.errorMessage, "is required.");
+    }
+
+    @Then("Empty message profile not updated")
+    public void emptyMessageProfileNotUpdated() {
+        Events.verifyContainsText(elements.succassesMessage, "");
+    }
+}
