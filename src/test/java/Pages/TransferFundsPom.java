e59c7f93948482d2e9c3ef422fda1730c11c98c2
UmutCanGuzel <umutcan0101010@gmail.com>
2023-10-06 14:37:23 +0300

    Transfer Funds

---
+++ src/test/java/Pages/TransferFundsPom.java
@@ -0,0 +1,33 @@
+package Pages;
+
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TransferFundsPom {
+
+    public TransferFundsPom(){
+        PageFactory.initElements(GWD.getDriver(),this);
+    }
+    @FindBy (id = "amount")
+    public WebElement amount;
+    @FindBy (id = "fromAccountId")
+    public WebElement fromAccount;
+    @FindBy (id = "toAccountId")
+    public WebElement toAccount;
+    @FindBy(xpath = "//input[@value='Transfer']")
+    public WebElement transferButton;
+    @FindBy(id = "amount")
+    public WebElement amountControl;
+    @FindBy (id = "fromAccountId")
+    public WebElement fromAccountControl;
+    @FindBy (id = "toAccountId")
+    public WebElement toAccountControl;
+    @FindBy (xpath = "(//table[@id='accountTable']//tr)[3]//a")
+    public WebElement sentAccount;
+    @FindBy(xpath = "//table[@id=\"transactionTable\"]/tbody/tr[5]/td[4]")
+    public WebElement amountSent;
+
+
+}
---
+++ src/test/java/StepDefinitions/TransferFunds.java
@@ -0,0 +1,41 @@
+package StepDefinitions;
+
+import Pages.AccountServices;
+import Pages.TransferFundsPom;
+import Utilities.Events;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TransferFunds {
+    AccountServices as = new AccountServices();
+    TransferFundsPom tfp=new TransferFundsPom();
+
+    @Given("The user must come to the transfer Funds transaction page")
+    public void theUserMustComeToTheTransferFundsTransactionPage() {
+        Events.click(as.transferFunds);
+    }
+
+    @When("The user must complete the Transfer Fund transaction")
+    public void theUserMustCompleteTheTransferFundTransaction()  {
+       Events.sendKeys(tfp.amount, "15");
+       Events.select(tfp.fromAccount).selectByIndex(0);
+       Events.select(tfp.toAccount).selectByIndex(1);
+       Events.click(tfp.transferButton);
+    }
+
+    @Then("User must verify Transfer Fund transaction")
+    public void userMustVerifyTransferFundTransaction() {
+        Events.verifyContainsText(tfp.amountControl,"15");
+        Events.verifyContainsText(tfp.fromAccountControl,"14565");
+        Events.verifyContainsText(tfp.toAccountControl,"17007");
+    }
+
+    @And("The user must check that the account sent from the account appears as credit")
+    public void theUserMustCheckThatTheAccountSentFromTheAccountAppearsAsCredit() {
+        Events.click(as.accountsOverview);
+        Events.click(tfp.sentAccount);
+        Events.verifyContainsText(tfp.amountSent,"$15");
+    }
+}
