bd5478843c2f9c1264e7208386a542874d23b846
Emrullah Tanıma <emrtnm@gmail.com>
2023-10-10 23:24:47 +0300

    Code review.

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -1,7 +1,5 @@
 package Utilities;
 
-import gherkin.lexer.Fi;
-import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
